dbad3b00f1c561f0fd54086383bd91fd7cdb6536
John Gartsu <jgartsu12@gmail.com>
2021-11-24 09:50:07 -0500

    add event listner that prints to console if click occurs

--- expandingcards/script.js
+++ expandingcards/script.js
@@ -1,30 +1,12 @@
 //js var that brings panels from html into js file
-// querySelectorAll is used to get grab all panel class divs and puts them in NodeList like an array
 const panels = document.querySelectorAll('.panel');
-console.log(panels);
-/* prints to console:
-    NodeList(5) [div.panel.active, div.panel, div.panel, div.panel, div.panel]
-    0: div.panel.active
-    1: div.panel
-    2: div.panel
-    3: div.panel
-    4: div.panel
-    length: 5
-    [[Prototype]]: NodeList 
-*/
-console.log(panels[0]); 
-/* prints the first element of the NodeList
-    div.panel.active
-*/
 
-// loop through node list
+// event listener on each panel DIV so if they are clicked they expand
 panels.forEach((panel) => {
-    console.log(panel);
+    // event listener listens for a click, when event happens run a function
+    panel.addEventListener('click', () => {
+        // when a click occurs on the page, prints this to console
+        console.log('eventlistener heard click');
+    });
+
 });
-/* return each div:
-    div.panel.active
-    div.panel
-    div.panel
-    div.panel
-    div.panel
-*/
