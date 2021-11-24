7c8cf8b0567ef22dbbfff35f9eb1bc7705dec867
John Gartsu <jgartsu12@gmail.com>
2021-11-24 15:46:45 -0500

    added notes to scroll animation

--- scrollAnimation/script.js
+++ scrollAnimation/script.js
@@ -2,18 +2,21 @@ const boxes = document.querySelectorAll('.box');
 
 window.addEventListener('scroll', checkBoxes);
 
-checkBoxes();
+checkBoxes(); //wont show until scroll without this here
 
+// check the position of each box, where is the trigger point where the box come in 
 function checkBoxes() {
+    // window.innerHeight gets inside height of the window - where trigger occurs
     const triggerBottom = window.innerHeight / 5 * 4;
+    // console.log(triggerBottom)
 
     boxes.forEach(box => {
-        const boxTop = box.getBoundingClientRect().top;
+        const boxTop = box.getBoundingClientRect().top; //gets DOM rect object - where in the viewport is it
 
         if (boxTop < triggerBottom) {
-            box.classList.add('show');
+            box.classList.add('show'); //scroll down
         } else {
-            box.classList.remove('show');
+            box.classList.remove('show'); //scroll up
         };
     });
 };
