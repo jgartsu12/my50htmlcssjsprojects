7493a215e107bcbc261e0e74a9dded2302b5f3ac
John Gartsu <jgartsu12@gmail.com>
2021-11-29 11:56:11 -0500

    added notes to event keycodes

--- eventKeyCodes/script.js
+++ eventKeyCodes/script.js
@@ -1,7 +1,9 @@
 const insert = document.getElementById('insert')
 
-// keyCode is deprecated
+// keyCode is deprecated, keydown = keystroke
 window.addEventListener('keydown', (event) => {
+    // console.log(event); ternary op needed for space so its not empty in 
+    // console.log(event.key);
   insert.innerHTML = `
   <div class="key">
   ${event.key === ' ' ? 'Space' : event.key} 
