7ce86b03f558342be9277746d903ce20af44af31
John Gartsu <jgartsu12@gmail.com>
2021-11-29 09:40:48 -0500

    added notes to sound board

--- soundBoard/script.js
+++ soundBoard/script.js
@@ -1,25 +1,27 @@
-const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong']
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
+// generate the button
 sounds.forEach(sound => {
-    const btn = document.createElement('button')
-    btn.classList.add('btn')
+    const btn = document.createElement('button');
+    btn.classList.add('btn'); //added for styling buttons
 
-    btn.innerText = sound
+    btn.innerText = sound; 
 
     btn.addEventListener('click', () => {
-        stopSongs()
-
-        document.getElementById(sound).play()
+        stopSongs();
+            // id is the sound name in the list of strings above const sounds
+        document.getElementById(sound).play();
     })
 
-    document.getElementById('buttons').appendChild(btn)
-})
+    document.getElementById('buttons').appendChild(btn); //style the buttons in css
+});
 
+// function stops sound last clicked sound if a new one is clicked
 function stopSongs() {
     sounds.forEach(sound => {
-        const song = document.getElementById(sound)
+        const song = document.getElementById(sound);
 
-        song.pause()
+        song.pause(); //no .stop() api for audio so just use pause() and set current time to 0
         song.currentTime = 0;
-    })
-}
+    });
+};
